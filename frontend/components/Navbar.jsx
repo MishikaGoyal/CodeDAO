4b672f8549144b84186bbe8446051de50e296d94
sj11105 <snehajain11105@gmail.com>
2024-12-02 11:18:33 +0530

    changes

--- frontend/components/Navbar.jsx
+++ frontend/components/Navbar.jsx
@@ -1,32 +1,44 @@
-import Link from "next/link"
+import Link from "next/link";
 
-export default function Navbar(){
-    return <nav className="bg-gray-800 p-4">
-    <div className="container mx-auto flex justify-between items-center">
-      <Link href="/" className="text-2xl font-semibold text-blue-300 hover:text-blue-100 transition-colors">
-        <h1 className="text-2xl font-bold text-blue-400">Code DAO</h1>
-        </Link> 
-      <ul className="flex space-x-4">
-        <li>
-          <Link href="/donor" className="text-blue-300 hover:text-blue-100 transition-colors">
-            Contribution Overview
-          </Link>
-        </li>
-        <li>
-          <Link
-            href="/open-proposals"
-            className="text-blue-300 hover:text-blue-100 transition-colors">
-            Open Proposals
-          </Link>
-        </li>
-        <li>
-          <Link
-            href="/accepted-proposals"
-            className="text-blue-300 hover:text-blue-100 transition-colors">
-            Accepted Proposals
-          </Link>
-        </li>
-      </ul>
-    </div>
-  </nav>
+export default function Navbar() {
+  return (
+    <nav className="bg-gray-800 p-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <Link
+          href="/"
+          className="text-2xl font-semibold text-blue-300 hover:text-blue-100 transition-colors"
+        >
+          <h1 className="text-2xl font-bold text-blue-400">
+            Code <span className="text-white">DAO</span>
+          </h1>
+        </Link>
+        <ul className="flex space-x-4">
+          <li>
+            <Link
+              href="/donor"
+              className="text-blue-300 hover:text-blue-100 transition-colors"
+            >
+              Contribution Overview
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/open-proposals"
+              className="text-blue-300 hover:text-blue-100 transition-colors"
+            >
+              Open Proposals
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/accepted-proposals"
+              className="text-blue-300 hover:text-blue-100 transition-colors"
+            >
+              Accepted Proposals
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
